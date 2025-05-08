c3dd5f5f90feff989044220e5f3ed4a062c104a9
Asier <asier.uriarte06@somo.eus>
2025-05-08 13:53:23 +0200

    Conexion hacia la otra base de datos

--- conexion.php
+++ conexion.php
@@ -1,4 +1,12 @@
 <?php
 // Conexión a la base de datos
-$conexion = mysqli_connect("127.0.0.1", "root", "", "bdprueba");
+$conexion = mysqli_connect("bibliotecamuskiz.cb2cweukck18.eu-north-1.rds.amazonaws.com", "admin", "d4rk1234", "bdbiblioteca", "3306");
+
+// Comprobación de conexión
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
 mysqli_set_charset($conexion, "utf8");
+
+echo "Conexión exitosa.\n";
